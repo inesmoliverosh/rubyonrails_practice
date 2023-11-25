807f3de244a8fcca5f7b6615cbf82e7decfec1e9
Inés María Oliveros H <ines.oliveros@symplifica.com>
2023-11-25 13:10:35 -0500

    controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
     def hello
         name = Pet.first.name
         breed = Pet.first.breed
-        @variable = "Hola mi mascota es #{name}, y es un #{breed}"
+        @variable = "Hola mi mascota es #{name}, y es un #{breed} blanco"
     end
 end
